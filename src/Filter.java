b312036cb483bf9134eae7c1011c5350a4af5e1e
preethi-r04 <preethiliki2004@gmail.com>
2026-02-13 14:54:22 +0530

    [Preethi] Added: UC2.4: Implements Stream filter program to extract even numbers

---
+++ src/Filter.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Filter {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(5, 10, 15, 20, 25, 30);
+
+        List<Integer> evenNumbers = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(evenNumbers);
+    }
+}
